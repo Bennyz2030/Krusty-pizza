f05377ac6a216b3148993040339bfeb0e37de163
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-04-08 11:34:11 -0700

    finish pizzaToppings function

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,36 @@ function Order(topping1, topping2, size) {
   this.price = 0;
 }
 
-Order.prototype.pizzaToppings = function() {
-  let toppingOne = this.topping1;
-  let toppingTwo = this.topping2;
-
-  if (toppingOne === "pepperoni") {
-    this.price += 4;
-  }
-  if (toppingTwo === "onion") {
-    this.price += 2;
-  }
-  return this.price;
-}
-
 Order.prototype.pizzaSize = function() {
   let zaSize = this.size;
 
   if (zaSize === "small") {
     this.price += 10;
+  }
+  return this.price;
+}
+
+Order.prototype.pizzaToppings = function() {
+  let toppingOne = this.topping1;
+  let toppingTwo = this.topping2;
+
+  if (toppingOne === "1") {
+    this.price += 4;
+  } else if (toppingOne === "2") {
+    this.price+= 2;
+  } else if (toppingOne === "3") {
+    this.price += 1000;
+  } else if (toppingOne === "4") {
+    this.price += 0;
+  }
+  if (toppingTwo === "1") {
+    this.price += 4;
+  } else if (toppingTwo === "2") {
+    this.price+= 2;
+  } else if (toppingTwo === "3") {
+    this.price += 1000;
+  } else if (toppingTwo === "4") {
+    this.price += 0;
   }
   return this.price;
 }
